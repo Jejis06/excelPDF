0c985eec8d07b0459cedc1de6d139288cbe1b4f2
Jejis06 <werex3233@gmail.com>
2024-07-24 17:08:30 +0200

    base data loading

--- app.py
+++ app.py
@@ -1,23 +1,59 @@
+# -*- coding: utf-8 -*-
+
 import tkinter as tk
 from tkinter import ttk
 import openpyxl
+import os
+import json
+
+doc_settings = {
+    "data_settings": {
+        "rangeStart": "A5",
+        "rangeEnd": "U28",
+        "filename": "Woda062024.xlsx",
+    },
+
+    "user_data_settings": {
+        "LOKATOR": "A",
+        "LOKAL_URZYTKOWY": "B",
+        "ZUZYCIE_WODY_ZIMNEJ": "G",
+        "ZUZYCIE_WODY_CIEPLEJ": "J",
+        "STAWKA_ZA_WODE_ZIMNA_I_SCIEKI": "L",
+        "ROZNICE_LICZNIKOW_ORAZ_CZESCI_WSPOLNE": "N",
+        "KOSZT_STALY_PODGRZANIA": "P",
+        "STAWKA_ZA_PODGRZANIE_WODY": "Q",
+    }
+}
 
 
-def load_data():
-    path = "C:\\Users\\ignac\\EXCELVIEWER\\dlugi.xlsx"
-    rangeStart = "A1"
-    rangeEnd = "C15"
+def get_col(cell: str, row: any):
+    return row[ord(cell) - ord('A')]
+
+
+def load_data(settings=None):
+    if settings is None:
+        settings = doc_settings
+    users = {}
+
+    filename = settings['data_settings']['filename']
+    rangeStart = settings['data_settings']['rangeStart']
+    rangeEnd = settings['data_settings']['rangeEnd']
+
+    path = os.path.join(os.getcwd(), filename)
 
     workbook = openpyxl.load_workbook(path)
-    sheet = workbook.active
+    sheetNames = workbook.sheetnames
+    sheet = workbook[sheetNames[0]]
 
+    rows = sheet[rangeStart + ":" + rangeEnd]
+    for row in rows:
+        user = {}
+        for key, val in settings['user_data_settings'].items():
+            user[key] = get_col(val, row).value
+        users[user["LOKATOR"]] = user
 
+    print(json.dumps(users, indent=4))
+    return users
 
-    data = sheet[rangeStart + ":" + rangeEnd]
-
-    for row in data:
-        for cell in row:
-            print(cell.value, end=' ')
-        print()
 
 load_data()
